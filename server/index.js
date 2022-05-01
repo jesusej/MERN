0573e75b60a2a5e16dc5879eae64d16c5aaa373e
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2022-05-01 00:07:37 -0700

    :hammer: New get function

--- server/index.js
+++ server/index.js
@@ -3,7 +3,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const UserModel = require("./models/Users");
+
 mongoose.connect(process.env.KEY);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(error);
+    } else {
+      res.json(result);
+    }
+  })
+});
 
 app.listen((process.env.PORT || 3001), () => {
   console.log("Server running");
